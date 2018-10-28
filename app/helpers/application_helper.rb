968d91c3214f951e1e2699adbeb816f96973010c
Nick-Damico <nickalan82@icloud.com>
2018-10-27 21:36:53 -0400

    refactor: Add application_helper#full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    app_title = "Ruby on Rails: Sample App"
+    page_title.empty? ? app_title : "#{page_title} | #{app_title}"
+  end
 end
