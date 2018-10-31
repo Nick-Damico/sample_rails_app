df1147e0a1ab08341ea5bf1074aa7f516ade0bf6
Nick-Damico <nickalan82@icloud.com>
2018-10-30 21:35:36 -0400

    Session#new form for 'login', #create action add

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(email: params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 end
