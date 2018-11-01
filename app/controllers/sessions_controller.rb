b53e05319c32f0e563afd358b6325c78e216413a
Nick-Damico <nickalan82@icloud.com>
2018-10-31 22:53:24 -0400

    Update valid Sessions#Create action, Test passing 'login with valid information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:password])
+    @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome back #{@user.name}."
+      redirect_to user_path(@user)
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,12 +1,28 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  setup do
+    @new_user = User.create!(name: 'Will', email: 'iamwill@valid.com',
+                                             password: 'IamWill',
+                                             password_confirmation: 'IamWill')
+  end
 
   test "login with invalid information" do
     get login_path
     assert_template "sessions/new"
     post login_path, params: {session: {name: "WilliamKitty", email: "w.kitty@invalid.com" }}
     assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: {session: {email: 'iamwill@valid.com', password: 'IamWill'}}
+    follow_redirect!
+    assert_template "users/show"
     assert_not flash.empty?
     get root_path
     assert flash.empty?
