d9c257b73ee47859e6a7c60d2178beb1a8e11345
Nick-Damico <nickalan82@icloud.com>
2018-11-02 10:41:00 -0400

    Add remember_digest supported methods .new_token,#remember,#authenticated?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = self.email.downcase }
   has_secure_password
   validates :name, presence: true,
@@ -10,9 +12,23 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: 6}
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                  BCrypt::Engine.cost
    BCrypt::Password.create(string, cost: cost)
  end
+
+ def self.new_token
+   SecureRandom.urlsafe_base64
+ end
+
+ def remember
+   self.remember_token = User.new_token
+   self.update_attribute(:remember_digest, User.digest(self.remember_token))
+ end
+ 
+ # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
