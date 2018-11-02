3a491281e2942b1e977c6b7670e23f96b4ebfcee
Nick-Damico <nickalan82@icloud.com>
2018-11-02 12:26:33 -0400

    Remember user functionality done. test supported

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       log_in(@user)
+      remember @user
       flash[:notice] = "Welcome back #{@user.name}."
       redirect_to @user
     else
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:notice] = "Successfully Logged Out"
     redirect_to root_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,9 +26,15 @@ class User < ApplicationRecord
    self.remember_token = User.new_token
    self.update_attribute(:remember_digest, User.digest(self.remember_token))
  end
- 
+
  # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if self.remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
